e7b83e12c64c7db6ce138cdd941fc92e8abc6da5
Jwizzed <101621268+Jwizzed@users.noreply.github.com>
2022-06-23 20:06:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jwizzed.github.io
+ Still not finished yet
---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/introduce")
+def introduce():
+    return render_template("introduce.html")
+
+@app.route("/dog")
+def dog():
+    return render_template('dog.html')
+
+@app.route("/photo")
+def photo():
+    return render_template('photo.html')
+
+
+if __name__=="__main__":
+    app.run(port=4990)
